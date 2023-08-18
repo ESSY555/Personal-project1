8c85dd4febbd158566a01ec88693372b7a97801e
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-08-18 08:59:34 +0100

    Update Navbar.jsx

--- ali-pay/src/components/Navbar.jsx
+++ ali-pay/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 <FaBars className={`${Visible?'hidden': 'block'}`} onClick={() => setVisible(prev => !prev)} fontSize={24} />
 <BiX className={`${Visible?'block': 'hidden'}`} onClick={() => setVisible(prev => !prev)} fontSize={32} />
         </div >
-        <div className={` ${Visible?'flex':'hidden'} w-[350px] px-[2rem] py-5  absolute flex-col h-[450px] bg-white shadow-lg -bottom-[470px] rounded-lg`}>
+        <div className={` ${Visible?'flex':'hidden'} w-[250px] px-[2rem] py-5  absolute flex-col min-h-[450px] bg-white shadow-lg -bottom-[470px] rounded-lg`}>
         <div className="flex flex-col items-start Poppins flex-1 justify-around ">
           <a href="#" onClick={() => setVisible(prev => !prev)} className="capitalize Poppins tracking-wide text-[24px]">Indiviual</a>
           <a href="#" onClick={() => setVisible(prev => !prev)} className="capitalize Poppins tracking-wide text-[24px]">Business</a>
@@ -42,8 +42,8 @@ const Navbar = () => {
         </div>
         <div className=" flex flex-col items-start gap-5 ">
           {" "}
-          <button className="px-[4rem] Poppins rounded-full text-primary-green bg-white font-bold tracking-wide border-primary-green border-2 py-3">Log in</button>
-          <button className="px-[4rem] Poppins text-white shadow-md font-bold tracking-wide rounded-full bg-primary-green py-3">Register</button>
+          <Link to={'/dashboard'} className="px-[4rem] Poppins rounded-full text-primary-green bg-white font-bold tracking-wide border-primary-green border-2 py-3">Log in</Link>
+          <Link to={'/dashboard'} className="px-[4rem] Poppins text-white shadow-md font-bold tracking-wide rounded-full bg-primary-green py-3">Register</Link>
         </div>
         </div>
       </div>
